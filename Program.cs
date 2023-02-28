fe10954ba2682dc18f15e33e6e5b43ac6968d856
estebandiaczun <esteban.diaczun@basisty.com>
2023-02-28 08:05:18 -0300

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese su nombre");
+
+            var nomb = (Console.ReadLine());
+
+            Console.WriteLine("Bienvendo "+nomb);
+
+            Console.WriteLine("Ingrese la hora actual");
+
+            var hora = (Console.ReadLine());
+
+            Console.WriteLine("La hora es :"+hora);
+
+
+
+        }
+    }
+}
