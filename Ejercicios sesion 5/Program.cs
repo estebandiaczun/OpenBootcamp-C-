8213e66753b594e79ce0f37281888a4edf404df8
estebandiaczun <esteban.diaczun@basisty.com>
2023-03-01 14:52:06 -0300

    Se agrega el ejercicio sesion 5

--- Ejercicios sesion 5/Program.cs
+++ Ejercicios sesion 5/Program.cs
@@ -72,6 +72,7 @@ namespace Program
                     case "1":
                     case "a":
                         Console.WriteLine($"Seleccionaste {Progamminglanguage[0]}");
+                        Console.WriteLine("Hola Mundo!");
                         break;
                     case "2":
                     case "b":
